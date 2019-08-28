bc8c8484e18ae6d93640b356f6c2188c8d40a189
B.Harish <balajiharish4@gmail.com>
2019-08-28 18:46:59 +0530

    Implemented DNS using udp

--- Comp. Networks Lab/DNS/dnsclient.c
+++ Comp. Networks Lab/DNS/dnsclient.c
@@ -1,14 +1,15 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <errno.h>
-#define PORT     8080 
-#define MAXLINE 1024 
+#define PORT     8080
+#define MAXLINE 1024
+#define backlog 2
 
 int createSocket(int *sockfd)
 {
@@ -26,30 +27,68 @@ void initServer(struct sockaddr_in *servaddr)
     (*servaddr).sin_family=AF_INET;
     (*servaddr).sin_addr.s_addr=INADDR_ANY;
     (*servaddr).sin_port=htons(PORT);
-}  
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    char *hello;//="Hello from cl" ;
+}
 
-    struct sockaddr_in     servaddr; 
-      
-    createSocket(&sockfd);
-    initServer(&servaddr);
-    
-    
+void sendMsg(int sockfd,char *hello,struct sockaddr_in *servaddr)
+{
+
     int n=0;
-    printf("\nClient: ");
-    gets(hello);
     n=sendto(sockfd,
 	(const char *)hello,strlen(hello),
-     MSG_CONFIRM,
-     (const struct sockaddr *)&servaddr,sizeof(servaddr));
-     
+     0,                                     //When MSG_CONFIRM/MSG_WAITALL they become same as that of write and read in TCP.//Using 0 here because MSG_COnfirm wont work in windows.
+     (const struct sockaddr *)servaddr,sizeof(*servaddr));
+}
+int receiveMsg(int sockfd,char *buffer,struct sockaddr_in *servaddr,int *len)
+{
+    int n=0;
+    n = recvfrom(sockfd,
+                (char *)buffer, MAXLINE,
+                0,                                    //When MSG_CONFIRM/MSG_WAITALL they become same as that of write and read in TCP.//Using 0 here because MSG_COnfirm wont work in windows.
+                 ( struct sockaddr *)servaddr,
+                len);
+    buffer[n]='\0';
+    return n;
+}//erecvMsg
 
-    
+void getIP(int sockfd,struct sockaddr_in *servaddr)
+{
+    int clients = 0,len=sizeof(servaddr);
+    char *buff=(char *)malloc(25*sizeof(char));
+    while(clients<backlog)
+    {
+        printf("\nClient - %d\n",clients+1);
+        printf("\n\tEnter the domain name: ");
+        gets(buff);
+        sendMsg(sockfd,buff,servaddr);
+        strcpy(buff,"");
+        receiveMsg(sockfd,buff,servaddr,&len);
+        if(strcmp(buff,"")!=0)
+        {
+            printf("\nServer: %s\n",buff);
+        }
+        else
+        {
+            printf("\nServer: No such domain name found\n");
+        }
 
-    close(sockfd);   
-    return 0; 
-} 
+        clients++;
+    }
+}
+
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    char *hello;//="Hello from cl" ;
+
+    struct sockaddr_in     servaddr;
+
+    createSocket(&sockfd);
+    initServer(&servaddr);
+
+    int n=0,len=0;
+
+    getIP(sockfd,&servaddr);
+
+    close(sockfd);
+    return 0;
+}
--- Comp. Networks Lab/DNS/dnsserver.c
+++ Comp. Networks Lab/DNS/dnsserver.c
@@ -1,14 +1,15 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#include<errno.h>  
-#define PORT     8080 
-#define MAXLINE 1024 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include<errno.h>
+#define PORT     8080
+#define MAXLINE 1024
+#define backlog 2
 
 
 int createSocket(int *sockfd)
@@ -27,20 +28,20 @@ void initServer(struct sockaddr_in *servaddr)
     (*servaddr).sin_family=AF_INET;
     (*servaddr).sin_addr.s_addr=INADDR_ANY;
     (*servaddr).sin_port=htons(PORT);
-}  
-  
+}
+
 void bindAndListen(int *sockfd,struct sockaddr_in *servaddr)
 {
     if(bind(*sockfd,(struct sockaddr*)servaddr,sizeof(*servaddr))<0)     //bind the socket with the local protocol addr
     perror("Bind Error");                                               //which is a combination of ip addr and port no.
 }
-// Driver code 
+// Driver code
 
 int checkIP(char *ip,char table[20][20],int domainOrIp)
 {														//table[evenNo]  --> domain name
 														//table[oddNo]   --> ip address
-	
-	for (int i = domainOrIp;  i < 20;  i += 2)
+
+	for (int i = domainOrIp;  i < 20 && table[i]!=NULL;  i += 2)
 	{
 		if(strcmp(ip,table[i])==0)
 		{
@@ -52,18 +53,20 @@ int makeTable(char table[20][20])
 {														//table[evenNo]  --> domain name
 														//table[oddNo]   --> ip address
 	int n=2;
-			char *dname,*ip;
+    char *dname,*ip;
+    dname=(char *)malloc(20*sizeof(char));
+    ip=(char *)malloc(20*sizeof(char));
 	int flag=0;
 	printf("\nEnter the no. of domain names you want to insert: ");
 //	scanf("%d",&n);
-	
+
 	int counter=0;
 	while(counter<=n)
 	{
 		DNAME:
-		printf("\nEnter domain name: ");		
+		printf("\nEnter domain name: ");
 		gets(dname);
-		
+
 		flag=checkIP(dname,table,0);
 		if(flag==1)
 		{
@@ -71,71 +74,130 @@ int makeTable(char table[20][20])
 			flag=0;
 			goto DNAME;
 		}
-		
+
 		GETIP:
 		printf("\nEnter the ip: ");
 		gets(ip);
-		
-		
-		flag=checkIP(ip,table,1);		
+
+        if(inet_addr(ip)==-1)
+        {
+            printf("\nInvalid IP\nEnter a valid IP\n");
+            goto GETIP;
+        }
+
+		flag=checkIP(ip,table,1);
 		if(flag==1)
 		{
 			printf("\nIp already exists\n");
-			flag=0;			
+			flag=0;
 			goto GETIP;
 		}
-		
+
+		dname[strlen(dname)]='\0';
+		ip[strlen(ip)]='\0';
 		strcpy(table[counter],dname);
 		strcpy(table[counter+1],ip);
+		strcpy(dname,"");
+		strcpy(ip,"");
 		counter+=2;
-		
+
 	}
-	
-	
+
+
 }//emakeTable
 void displayTable(char table[20][20])
 {
 	int i=0;
 	printf("\nDomain Name\tIP address\n");
-	
+
 	for (i = 0; i < 19; i += 2)
 	{
-		if(table[i]==NULL)
-		{
-			
-			continue;
-		}	
 		printf("%s\t%s\n",table[i],table[i+1]);
-		
+
 	}
 }
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
+
+int receiveMsg(int sockfd,char *buffer,struct sockaddr_in *cliaddr,int *len)
+{
+    int n=0;
+    n = recvfrom(sockfd,
+                (char *)buffer, MAXLINE,
+                0,                                    //When MSG_CONFIRM/MSG_WAITALL they become same as that of write and read in TCP.//Using 0 here because MSG_COnfirm wont work in windows.
+                 ( struct sockaddr *)cliaddr,
+                len);
+    buffer[n]='\0';
+    return n;
+}//erecvMsg
+
+void lookUp(char table[20][20],char *dname)
+{
+    for(int i=0;i<19;i+=2)
+    {
+        //printf("\nstr = %s\ndname = %s\n%d\n",table[i],dname,strcmp(dname,table[i]));
+        if(strcmp(dname,table[i])==0)
+        {
+            strcpy(dname,table[i+1]);
+            return;
+        }
+    }
+    strcpy(dname,"");       //empty the buffer to signal that no value found.
+}
+void sendMsg(int sockfd,char *hello,struct sockaddr_in *cliaddr)
+{
+
+    int n=0;
+    n=sendto(sockfd,
+	(const char *)hello,strlen(hello),
+     0,                                     //When MSG_CONFIRM/MSG_WAITALL they become same as that of write and read in TCP.//Using 0 here because MSG_COnfirm wont work in windows.
+     (const struct sockaddr *)cliaddr,sizeof(*cliaddr));
+}
+
+void recvDomainName(int sockfd,struct sockaddr_in *cliaddr,int *len,char table[20][20])
+{
+    int n=0;
+    char *buff=(char *)malloc(25*sizeof(char));
+
+    int clients=0;
+
+    while(clients<backlog)
+    {
+        n=receiveMsg(sockfd,buff,cliaddr,len);
+        buff[n]='\0';
+        printf("\nClient: Requesting domain : \'%s\'\n",buff);
+        lookUp(table,buff);
+        sendMsg(sockfd,buff,cliaddr);
+        printf("\nReply sent\n");
+        clients++;
+    }
+}
+
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
     char *hello ;
     hello=(char * )malloc(100*sizeof(char));
 
-    struct sockaddr_in servaddr, cliaddr; 
-      
-    bzero(&cliaddr,sizeof(cliaddr));
-    
+    struct sockaddr_in servaddr, cliaddr;
+
     createSocket(&sockfd);
     initServer(&servaddr);
     bindAndListen(&sockfd,&servaddr);
-      
+
 	int n=0,len;
 	char table[20][20];
-	
+
+	bzero(table,sizeof(table));
+
 	makeTable(table);
-	displayTable(table);
- 	
- 	n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
-                &len); 
-    buffer[n]='\0';
-	printf("\nServer: %s\n",buffer);              
-    
+	//displayTable(table);
+
+    len = sizeof(cliaddr);
+
+    recvDomainName(sockfd,&cliaddr,&len,table);
+
+    //sendMsg(sockfd,buffer,&cliaddr);
+
     close(sockfd);
-      
-    return 0; 
-} 
+
+    return 0;
+}
